57f2c6313ea55ed53c8258b651829b99c6350ead
Arman Bilge <armanbilge@gmail.com>
2022-08-23 07:10:08 +0000

    Resetting a bit :(

--- core/src/main/scala/epollcat/EpollApp.scala
+++ core/src/main/scala/epollcat/EpollApp.scala
@@ -16,30 +16,13 @@
 
 package epollcat
 
-import cats.effect.IO
 import cats.effect.IOApp
-import cats.effect.std.Console
 import cats.effect.unsafe.IORuntime
-import epollcat.unsafe.EpollExecutorScheduler
 import epollcat.unsafe.EpollRuntime
 
 trait EpollApp extends IOApp {
 
   override final lazy val runtime: IORuntime = EpollRuntime(runtimeConfig)
-
-  implicit final lazy val epoll: Epoll[IO] =
-    Epoll(runtime.compute.asInstanceOf[EpollExecutorScheduler])
-
-  implicit final lazy val console: Console[IO] =
-    instances
-      .console
-      .epollConsole(IO.asyncForIO, IO.consoleForIO, epoll)
-      .allocated
-      .map(_._1)
-      .syncStep(runtimeConfig.autoYieldThreshold)
-      .unsafeRunSync() // craziness
-      .toOption
-      .get
 
 }
 
