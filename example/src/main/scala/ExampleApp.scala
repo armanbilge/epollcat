a6b2a93a5e7405eb05b8cb3d849cf3f3a75c3754
Arman Bilge <armanbilge@gmail.com>
2022-09-11 16:06:21 +0000

    Create example app, it hangs :(

---
+++ example/src/main/scala/ExampleApp.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package epollcat
+
+import cats.effect.IO
+
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+
+object ExampleApp extends EpollApp.Simple {
+
+  def decode(bb: ByteBuffer): String =
+    StandardCharsets.UTF_8.decode(bb).toString()
+
+  override def run: IO[Unit] = {
+    val address = new InetSocketAddress(InetAddress.getByName("postman-echo.com"), 80)
+    val bytes =
+      """|GET /get HTTP/1.1
+         |Host: postman-echo.com
+         |
+         |""".stripMargin.getBytes()
+
+    IOSocketChannel.open.use { ch =>
+      for {
+        _ <- ch.connect(address)
+        _ <- ch.write(ByteBuffer.wrap(bytes))
+        bb <- IO(ByteBuffer.allocate(1024))
+        _ <- ch.read(bb)
+        res <- IO(bb.position(0)) *> IO(decode(bb))
+        _ <- IO.println(res)
+      } yield ()
+    }
+  }
+
+}
