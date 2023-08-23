43a4ae77af3c0d446d7798067f4876a1425e428e
Arman Bilge <armanbilge@gmail.com>
2023-08-23 04:11:05 +0000

    Formatting

--- core/src/main/scala/epollcat/unsafe/KqueueExecutorScheduler.scala
+++ core/src/main/scala/epollcat/unsafe/KqueueExecutorScheduler.scala
@@ -129,11 +129,11 @@ private[unsafe] final class KqueueExecutorScheduler(
         EvAdd(fd, EVFILT_WRITE, cb)
       else null
 
-    if (readEvent != null){
+    if (readEvent != null) {
       changes.add(readEvent)
       ()
     }
-    if (writeEvent != null){
+    if (writeEvent != null) {
       changes.add(writeEvent)
       ()
     }
